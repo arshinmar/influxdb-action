20b85b3cf74139972e1bcd7c83a0d51277ed04dd
arshinmar <arsh.kadakia18@gmail.com>
2021-08-10 12:26:47 -0400

    added echo statements

--- install-influxdb.sh
+++ install-influxdb.sh
@@ -10,8 +10,12 @@ else
   export INFLUXDB_DOWNLOAD_PATH="https://dl.influxdata.com/influxdb/releases"
 fi
 wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb-$INFLUXDB_VERSION-static_linux_amd64.tar.gz
+echo '1'
 tar xvfz influxdb-$INFLUXDB_VERSION-static_linux_amd64.tar.gz
+echo '2'
 sudo cp influxdb-$INFLUXDB_VERSION-1/influxd /usr/local/bin/
+echo '3'
 rm -r influxdb-$INFLUXDB_VERSION-1/
-
+echo '4'
 cd -
+echo '5'
