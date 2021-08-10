a0529f2fa7055f982755e175349f58597b0aa83e
Arsh Kadakia <53918384+arshinmar@users.noreply.github.com>
2021-08-10 13:32:55 -0400

    Update setup-influxdb.sh

--- setup-influxdb.sh
+++ setup-influxdb.sh
@@ -1,11 +1,5 @@
 
 if [ "$INFLUXDB_START" = "true" ]
 then
-    influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
-    until curl -s http://localhost:8086/health; do sleep 1; done
-    influx setup --host http://localhost:8086 -f \
-        -o $INFLUXDB_ORG \
-        -u $INFLUXDB_USER \
-        -p $INFLUXDB_PASSWORD \
-        -b $INFLUXDB_BUCKET
+    influxd --config /etc/influxdb/influxdb.conf
 fi
